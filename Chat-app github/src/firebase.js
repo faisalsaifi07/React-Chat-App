b7bdc4cebe4b894a15710f1ef5ce319183f78bb6
Mohd Faisal <108757394+faisalsaifi07@users.noreply.github.com>
2022-09-18 00:33:15 +0530

    Add files via upload

---
+++ Chat-app github/src/firebase.js
@@ -0,0 +1,36 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+import { getAuth } from "firebase/auth";
+import { getFirestore } from "firebase/firestore";
+import { getStorage } from "firebase/storage";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+// const firebaseConfig = {
+//   apiKey: process.env.REACT_APP_API_KEY,
+//   authDomain: process.env.REACT_APP_AUTH_DOMAIN,
+//   databaseURL: process.env.REACT_APP_DATABASE_URL,
+//   projectId: process.env.REACT_APP_PROJECT_ID,
+//   storageBucket: process.env.REACT_APP_STORAGE_BUCKET,
+//   messagingSenderId: process.env.REACT_APP_MESSAGING_SENDER_ID,
+//   appId: process.env.REACT_APP_APP_ID,
+// };
+
+const firebaseConfig = {
+  apiKey: "AIzaSyBTAKnNpCwdRFted7i3WBonVfGQRCXF83M",
+  authDomain: "react-chat-app-cc73a.firebaseapp.com",
+  projectId: "react-chat-app-cc73a",
+  storageBucket: "react-chat-app-cc73a.appspot.com",
+  messagingSenderId: "126748015501",
+  appId: "1:126748015501:web:afe934c20f72c6ed759bc4"
+};
+
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
+const auth = getAuth(app);
+const db = getFirestore(app);
+const storage = getStorage(app);
+
+export { auth, db, storage };
